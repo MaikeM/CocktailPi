544e313aaf92c256f0c14df39b63d824690a7ee9
Manuel Zapp <manuelzapp@manuels-mbp-2.speedport_w_723v_1_37_000>
2015-09-03 09:31:58 +0200

    Added template and cocktail view

--- cocktail/cocktail/urls.py
+++ cocktail/cocktail/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^', include('cocktaildb.urls', namespace='cocktaildb')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- cocktail/cocktaildb/views.py
+++ cocktail/cocktaildb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Cocktail
 
 # Create your views here.
+def index(request):
+	cocktails = Cocktail.objects.all()
+	context = RequestContext(request, {
+	        'cocktails': cocktails,
+	})
+	return render(request, 'cocktaildb/cocktails.html', context)
