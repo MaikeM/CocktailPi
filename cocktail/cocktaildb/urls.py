c4949020ce199b98007a8059c04f06291735abca
Maike <maikemaas@gmx.net>
2015-09-03 14:52:32 +0200

    Zutaten und Step Liste

--- cocktail/cocktaildb/urls.py
+++ cocktail/cocktaildb/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^cocktail/(?P<cocktail_id>[0-9]+)/(?P<step>[0-9]+)/$', views.cocktail, name='cocktail'),
+    url(r'^ingredient/(?P<ingredient_id>[0-9]+)/$', views.ingredient, name='ingredient'),
+    url(r'^ingredients', views.ingredients, name='ingredients'),
 ]
--- cocktail/cocktaildb/views.py
+++ cocktail/cocktaildb/views.py
@@ -2,12 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from .models import Cocktail
+from .models import Cocktail, MixStep, Ingredient
 
 # Create your views here.
 def index(request):
-	cocktails = Cocktail.objects.all()
+	cocktails = Cocktail.objects.all().order_by('name')
+	steps = MixStep.objects.all().order_by('ingredient__name')
+	ingredients = Ingredient.objects.all().order_by('name')
 	context = RequestContext(request, {
 	        'cocktails': cocktails,
+	        'ingredients': ingredients,
+	        'steps': steps,
 	})
 	return render(request, 'cocktaildb/cocktails.html', context)
+
+def ingredient(request, ingredient_id):
+	try:
+		ingredient = Ingredient.objects.get(pk=ingredient_id)
+		context = RequestContext(request, {
+	        'ingredient': ingredient,
+	})
+	except Ingredient.DoesNotExist:
+		raise Http404("Ingredient does not exist")
+	return render(request, 'cocktaildb/ingredient.html', context)
+
+def cocktail(request, cocktail_id, step):
+	try:
+		cocktail = Cocktail.objects.get(pk=cocktail_id)
+		mixstep = MixStep.objects.get(cocktail_id=cocktail, step = step) 
+		a = int(step)
+		a = a+1
+		context = RequestContext(request, {
+	        'cocktail': cocktail, 
+	        'mixstep': mixstep,
+	        'next_step': a
+		})
+	except Cocktail.DoesNotExist:
+		raise Http404("Cocktail does not exist")
+	return render(request, 'cocktaildb/cocktail.html', context)
+
+
+def ingredients(request):
+	ingredients = Ingredient.objects.all().order_by('name')
+	context = RequestContext(request, {
+	        'ingredients': ingredients,
+	})
+	return render(request, 'cocktaildb/ingredients.html', context)
